a8ebe689a9a66ec76683dfc182474452a418eb99
seanMooney1996 <seanmooney1996@gmail.com>
2024-05-02 12:40:13 +0100

    small changes to login livewire

--- app/Http/Livewire/Login.php
+++ app/Http/Livewire/Login.php
@@ -2,12 +2,31 @@
 
 namespace App\Http\Livewire;
 
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class Login extends Component
 {
+
+    public $email;
+    public $password;
     public function render()
     {
+
         return view('livewire.login');
+    }
+
+    public function login()
+    {
+        $credentials = [
+            'email' => $this->email,
+            'password' => $this->password,
+        ];
+
+        if (Auth::attempt($credentials)) {
+            return redirect()->intended('/dashboard');
+        } else {
+            session()->flash('error', 'Invalid credentials. Please try again.');
+        }
     }
 }
