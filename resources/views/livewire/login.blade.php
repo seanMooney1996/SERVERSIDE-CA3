bb6376903eb5de405f0bc37be7713f017c0fdfda
seanMooney1996 <seanmooney1996@gmail.com>
2024-05-02 12:50:02 +0100

    Small changes

--- resources/views/livewire/login.blade.php
+++ resources/views/livewire/login.blade.php
@@ -1,3 +1,20 @@
 <div>
-    {{-- The best athlete wants his opponent at his best. --}}
+    <div>
+        <form wire:submit.prevent="login">
+            @if (session()->has('error'))
+            <div>{{ session('error') }}</div>
+            @endif
+            <div>
+                <label>Email</label>
+                <input type="email" wire:model="email" required>
+            </div>
+            <div>
+                <label>Password</label>
+                <input type="password" wire:model="password" required>
+            </div>
+            <div>
+                <button type="submit">Login</button>
+            </div>
+        </form>
+    </div>
 </div>
