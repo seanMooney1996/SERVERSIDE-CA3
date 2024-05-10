2960a0209c184090521c52e16707b6130e97f111
NoahKrobot <D00259927@student.dkit.ie>
2024-05-10 13:21:20 +0100

    Login and register pages edit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
 <x-guest-layout>
 
 
-    <x-authentication-card>
+    <div class="loginBox">
         <x-slot name="logo">
         </x-slot>
 
@@ -23,13 +23,13 @@
             @csrf
 
             <div>
-                <x-label for="email" value="{{ __('Email') }}" />
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+                <label for="email" value="{{ __('Email') }}" >Email</label>
+                <x-input id="email" class="emailContainer" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
             </div>
 
-            <div class="mt-4">
-                <x-label for="password" value="{{ __('Password') }}" />
-                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
+            <div >
+                <label for="password" value="{{ __('Password') }}">Password</label>
+                <x-input id="password" class="emailContainer" type="password" name="password" required autocomplete="current-password" />
             </div>
 
             <div class="block mt-4">
@@ -40,16 +40,11 @@
             </div>
 
             <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
 
-                <x-button class="ml-4">
+                <button class="LoginButton">
                     {{ __('Log in') }}
-                </x-button>
+                </button>
             </div>
         </form>
-    </x-authentication-card>
+    </div>
 </x-guest-layout>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,8 @@
 <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 @include('navbar')
 <x-guest-layout>
-    <x-authentication-card>
+<div class="loginBox">
+
         <x-slot name="logo">
             <x-authentication-card-logo />
         </x-slot>
@@ -12,22 +13,22 @@
             @csrf
 
             <div>
-                <x-label for="name" value="{{ __('Name') }}" />
+                 <label for="name" value="{{ __('Name') }}" >Name</label>
                 <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
             </div>
 
             <div class="mt-4">
-                <x-label for="email" value="{{ __('Email') }}" />
+                <label for="email" value="{{ __('Email') }}" >Email</label>
                 <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
             </div>
 
             <div class="mt-4">
-                <x-label for="password" value="{{ __('Password') }}" />
+                <label for="password" value="{{ __('Password') }}" >Password</label>
                 <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
             </div>
 
             <div class="mt-4">
-                <x-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
+                <label for="password_confirmation" value="{{ __('Confirm Password') }}" >Confirm Password</label>
                 <x-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
             </div>
 
@@ -49,14 +50,12 @@
             @endif
 
             <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
-                </a>
+               
 
-                <x-button class="ml-4">
+                <button class="LoginButton">
                     {{ __('Register') }}
-                </x-button>
+                </button>
             </div>
         </form>
-    </x-authentication-card>
+                                </div>
 </x-guest-layout>
